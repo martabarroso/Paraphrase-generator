4e1ac81f6873ca9b350aa646af4c021eaff849b2
Marta Barroso <mbarrosoisidoro@gmail.com>
2020-11-22 22:30:22 +0100

    Add runner class
    The baseline is alredy working.
    
    Implements #2

--- evaluation/sentence_classifier.py
+++ evaluation/sentence_classifier.py
@@ -1,10 +1,13 @@
 from evaluation.model import TextClassifier
 from evaluation.preprocessing import Preprocessing
 from evaluation.configuration import CONFIGURATION
+from evaluation.run import Run
 
 
 if __name__ == '__main__':
-    pr = Preprocessing(CONFIGURATION['num_words'], CONFIGURATION['seq_len']).preprocess()
+    data = Preprocessing(CONFIGURATION['num_words'], CONFIGURATION['seq_len']).preprocess()
 
     # Initialize the model
     model = TextClassifier(CONFIGURATION)
+
+    Run().train(model, data, CONFIGURATION)
