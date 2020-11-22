96b73d669a5fb72ba538b5f1510339c6b1234ddf
Marta Barroso <mbarrosoisidoro@gmail.com>
2020-11-22 22:19:03 +0100

    Update sentence classifier main to initialize the model
    Implements #2

--- evaluation/sentence_classifier.py
+++ evaluation/sentence_classifier.py
@@ -1,7 +1,10 @@
+from evaluation.model import TextClassifier
 from evaluation.preprocessing import Preprocessing
 from evaluation.configuration import CONFIGURATION
 
 
 if __name__ == '__main__':
     pr = Preprocessing(CONFIGURATION['num_words'], CONFIGURATION['seq_len']).preprocess()
-    print(pr)
+
+    # Initialize the model
+    model = TextClassifier(CONFIGURATION)
