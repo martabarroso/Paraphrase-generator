928330f38bb827cbccac9abbf207fced8e26e376
Marta Barroso <mbarrosoisidoro@gmail.com>
2020-12-29 14:34:38 -0300

    Update preprocessing in sentence classifier main

--- evaluation/sentence_classifier.py
+++ evaluation/sentence_classifier.py
@@ -1,13 +1,28 @@
 from evaluation.model import TextClassifier
-from evaluation.preprocessing import Preprocessing
+from evaluation.preprocessing import Preprocessing, join_data_and_paraphrases
 from evaluation.configuration import CONFIGURATION
 from evaluation.run import Run
 
 
 if __name__ == '__main__':
-    data = Preprocessing(CONFIGURATION['num_words'], CONFIGURATION['seq_len'], './input.csv').preprocess()
+    input_path = '../resources/train.csv'
+    paraphrases = {'en': [['When they go on field trips, they let parents go and help.',
+                           'Here we can only take one bus.'],
+                          ['When they go on excursions, they let their parents go and help.',
+                           'Here we can only take a bus.']],
+                   'de': [['Wenn sie auf Exkursionen gehen, lassen sie die Eltern gehen und helfen.',
+                           'Hier können wir nur einen Bus nehmen.']]}
+    language = 'en'
+
+    # Perform data augmentation
+    df = join_data_and_paraphrases(input_path, paraphrases, language='en')
 
     # Initialize the model
     model = TextClassifier(CONFIGURATION)
 
+    # Preprocessing
+    preprocessing = Preprocessing(CONFIGURATION['num_words'], CONFIGURATION['seq_len'], df)
+    data = preprocessing.preprocess()
+
+    # Training
     Run().train(model, data, CONFIGURATION)
