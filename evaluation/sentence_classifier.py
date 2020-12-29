008de3bd1d249997563edc4c58ea904c2d98037d
Marta Barroso <mbarrosoisidoro@gmail.com>
2020-12-29 15:02:12 -0300

    Update sentence classifier to make experiments with ag news dataset

--- evaluation/sentence_classifier.py
+++ evaluation/sentence_classifier.py
@@ -3,9 +3,12 @@ from evaluation.preprocessing import Preprocessing, join_data_and_paraphrases
 from evaluation.configuration import CONFIGURATION
 from evaluation.run import Run
 
+import pandas as pd
+
 
 if __name__ == '__main__':
-    input_path = '../resources/train.csv'
+    paraphrases = None
+    '''
     paraphrases = {'en': [['When they go on field trips, they let parents go and help.',
                            'Here we can only take one bus.'],
                           ['When they go on excursions, they let their parents go and help.',
@@ -13,9 +16,13 @@ if __name__ == '__main__':
                    'de': [['Wenn sie auf Exkursionen gehen, lassen sie die Eltern gehen und helfen.',
                            'Hier können wir nur einen Bus nehmen.']]}
     language = 'en'
+    '''
 
-    # Perform data augmentation
-    df = join_data_and_paraphrases(input_path, paraphrases, language='en')
+    df_train = pd.read_csv('../input/train.csv')
+    df_test = pd.read_csv('../input/test.csv')
+    df = pd.concat([df_train, df_test], axis=0, join='outer')
+    df = join_data_and_paraphrases(df, paraphrases, language='en')
+
 
     # Initialize the model
     model = TextClassifier(CONFIGURATION)
