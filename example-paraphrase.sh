c12cbc6d190e10dce0ab4b68ff539d685eb611cc
Jordi Armengol <me@jordiae.com>
2021-01-05 01:18:40 +0100

    Added experiments script

--- example-paraphrase.sh
+++ example-paraphrase.sh
@@ -2,7 +2,7 @@
 
 # Dummy (identity)
 
-python paraphrase.py --method dummy --translators fair-wmt19-en-de fair-wmt19-de-en --input input/example.txt --n 1
+python paraphrase.py --method dummy --translators fair-wmt19-en-de fair-wmt19-de-en --input input/example.txt
 
 # Round-trip
-python paraphrase.py --method roundtrip --translators fair-wmt19-en-de fair-wmt19-de-en --input input/example.txt --n 1
+python paraphrase.py --method roundtrip --translators fair-wmt19-en-de fair-wmt19-de-en --input input/example.txt
---
+++ run-experiments.sh
@@ -0,0 +1,27 @@
+##!/usr/bin/env bash
+
+# Roundtrip:
+
+# German
+python paraphrase.py --method roundtrip --translators fair-wmt19-en-de fair-wmt19-de-en --input input/tweets.txt
+
+# Russian
+python paraphrase.py --method roundtrip --translators fair-wmt19-en-ru fair-wmt19-ru-en --input input/tweets.txt
+
+# Tamil
+python paraphrase.py --method roundtrip --translators fair-wmt20-en-ta fair-wmt20-ta-en --input input/tweets.txt
+
+# Speech
+python paraphrase.py --method roundtrip --translators silero_asr_en tacotron_pytorch --input input/tweets.txt
+
+# All
+python paraphrase.py --method roundtrip --translators fair-wmt19-en-de fair-wmt19-de-en fair-wmt19-en-ru \
+       fair-wmt19-ru-en fair-wmt20-en-ta fair-wmt20-ta-en silero_asr_en tacotron_pytorch --input input/tweets.txt
+
+####
+
+# N-cycles:
+
+# All
+python paraphrase.py --method ncycles --translators fair-wmt19-en-de fair-wmt19-de-en fair-wmt19-en-ru \
+       fair-wmt19-ru-en fair-wmt20-en-ta fair-wmt20-ta-en silero_asr_en tacotron_pytorch --input input/tweets.txt
