824cb3927bb97a7061dfd4bff6f2ae80458867cd
Jordi Armengol <me@jordiae.com>
2021-01-05 16:24:23 +0100

    Added experiments script

--- run-experiments.sh
+++ run-experiments.sh
@@ -4,24 +4,24 @@
 
 # German
 echo "german"
-python paraphrase.py --method roundtrip --translators fair-wmt19-en-de fair-wmt19-de-en --input input/tweets_.csv
+python paraphrase.py --method roundtrip --translators fair-wmt19-en-de fair-wmt19-de-en --input input/tweets.csv
 
 # Russian
 echo "russian"
-python paraphrase.py --method roundtrip --translators fair-wmt19-en-ru fair-wmt19-ru-en --input input/minitweets.csv
+python paraphrase.py --method roundtrip --translators fair-wmt19-en-ru fair-wmt19-ru-en --input input/tweets.csv
 
 # Tamil
 echo "tamil"
-python paraphrase.py --method roundtrip --translators fair-wmt20-en-ta fair-wmt20-ta-en --input input/minitweets.csv
+python paraphrase.py --method roundtrip --translators fair-wmt20-en-ta fair-wmt20-ta-en --input input/tweets.csv
 
 # Speech
 echo "speech"
-python paraphrase.py --method roundtrip --translators silero_asr_en tacotron_pytorch --input input/minitweets.csv
+python paraphrase.py --method roundtrip --translators silero_asr_en tacotron_pytorch --input input/tweets.csv
 
 # All
 echo "all"
 python paraphrase.py --method roundtrip --translators fair-wmt19-en-de fair-wmt19-de-en fair-wmt19-en-ru \
-       fair-wmt19-ru-en fair-wmt20-en-ta fair-wmt20-ta-en silero_asr_en tacotron_pytorch --input input/minitweets.csv
+       fair-wmt19-ru-en fair-wmt20-en-ta fair-wmt20-ta-en silero_asr_en tacotron_pytorch --input input/tweets.csv
 
 ####
 
@@ -30,4 +30,4 @@ python paraphrase.py --method roundtrip --translators fair-wmt19-en-de fair-wmt1
 # All
 echo "ncycles"
 python paraphrase.py --method ncycles --translators fair-wmt19-en-de fair-wmt19-de-en fair-wmt19-en-ru \
-       fair-wmt19-ru-en fair-wmt20-en-ta fair-wmt20-ta-en silero_asr_en tacotron_pytorch --input input/minitweets.csv
+       fair-wmt19-ru-en fair-wmt20-en-ta fair-wmt20-ta-en silero_asr_en tacotron_pytorch --input input/tweets.csv
