4e37fadd79108c3ce91356c7f50ae5b0b19b6bf5
Jordi Armengol <me@jordiae.com>
2021-01-05 03:37:40 +0100

    Added experiments script

--- run-experiments.sh
+++ run-experiments.sh
@@ -3,20 +3,20 @@
 # Roundtrip:
 
 # German
-python paraphrase.py --method roundtrip --translators fair-wmt19-en-de fair-wmt19-de-en --input input/tweets.txt
+python paraphrase.py --method roundtrip --translators fair-wmt19-en-de fair-wmt19-de-en --input input/tweets.csv
 
 # Russian
-python paraphrase.py --method roundtrip --translators fair-wmt19-en-ru fair-wmt19-ru-en --input input/tweets.txt
+python paraphrase.py --method roundtrip --translators fair-wmt19-en-ru fair-wmt19-ru-en --input input/tweets.csv
 
 # Tamil
-python paraphrase.py --method roundtrip --translators fair-wmt20-en-ta fair-wmt20-ta-en --input input/tweets.txt
+python paraphrase.py --method roundtrip --translators fair-wmt20-en-ta fair-wmt20-ta-en --input input/tweets.csv
 
 # Speech
-python paraphrase.py --method roundtrip --translators silero_asr_en tacotron_pytorch --input input/tweets.txt
+python paraphrase.py --method roundtrip --translators silero_asr_en tacotron_pytorch --input input/tweets.csv
 
 # All
 python paraphrase.py --method roundtrip --translators fair-wmt19-en-de fair-wmt19-de-en fair-wmt19-en-ru \
-       fair-wmt19-ru-en fair-wmt20-en-ta fair-wmt20-ta-en silero_asr_en tacotron_pytorch --input input/tweets.txt
+       fair-wmt19-ru-en fair-wmt20-en-ta fair-wmt20-ta-en silero_asr_en tacotron_pytorch --input input/tweets.csv
 
 ####
 
@@ -24,4 +24,4 @@ python paraphrase.py --method roundtrip --translators fair-wmt19-en-de fair-wmt1
 
 # All
 python paraphrase.py --method ncycles --translators fair-wmt19-en-de fair-wmt19-de-en fair-wmt19-en-ru \
-       fair-wmt19-ru-en fair-wmt20-en-ta fair-wmt20-ta-en silero_asr_en tacotron_pytorch --input input/tweets.txt
+       fair-wmt19-ru-en fair-wmt20-en-ta fair-wmt20-ta-en silero_asr_en tacotron_pytorch --input input/tweets.csv
