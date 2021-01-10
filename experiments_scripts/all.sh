3934c55fd8ebf8fef54545791095ef6283bec366
Jordi Armengol Estapé <me@jordiae.com>
2021-01-10 22:34:53 +0100

    Delete all.sh

--- experiments_scripts/all.sh
+++
@@ -1,8 +0,0 @@
-##!/usr/bin/env bash
-
-# Roundtrip:
-
-# All
-echo "all"
-python paraphrase.py --method roundtrip --translators marian-en-de marian-de-en marian-en-ru \
-       marian-ru-en marian-en-is marian-is-en silero_asr_en tacotron_pytorch --input input/tweets.csv
